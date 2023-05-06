74c518918a2a188f617ae756802098c29f8ebf2b
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-05-05 23:09:40 -0400

    BasePluginSetup

---
+++ MuckMobSpawner/MuckMobSpawner/Main.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace MuckMobSpawner
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    internal class Main : BaseUnityPlugin
+    {
+        internal const string
+            GUID = "crafterbot.mobspawner",
+            NAME = "MuckMobSpawner",
+            VERSION = "1.0.0",
+            GIT_VERSION = "";
+    }
+}
